02ad8dfc6a3c71a7c5af02b2fb34afe2f2132530
Александра Долгих <dolgihas04@yandex.ru>
2023-10-02 19:41:02 +0500

    Добавляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,24 @@
-function checkLength (string, length){
+var checkLength = function (string, length){
   if (string.length<=length){
     return true;
-  }
-  return false;
-}
+  };
 
-function isPalindrom (string){
+  return false;
+};
+
+var isPalindrom = function (string){
   let stringByContrast = '';
   for (let i=(string.length-1); i>=0; i--){
     stringByContrast += string[i];
-  }
+  };
 
   if (string === stringByContrast){
+
     return true;
-  }
+  };
+
   return false;
-}
+};
+
+checkLength('кот', 4);
+isPalindrom('кот');
