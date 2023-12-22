fd51f1121f3e65452e36d935ee1e9558e6218887
Александра Долгих <dolgihas04@yandex.ru>
2023-12-22 11:34:57 +0500

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ import { renderGallery } from './gallery.js';
 import { openEditPopup, setPopupSubmit, closePopup } from './open-big-picture.js';
 import { getData } from './api.js';
 import { showAlert } from './utils.js';
+import { initFilters } from './filtration-image.js';
+import { debounce } from './utils.js';
 
 getData()
   .then((thumbnails) => {
+    const debounceRenderGallery = debounce(renderGallery);
     renderGallery(thumbnails);
+    initFilters(thumbnails, debounceRenderGallery);
   })
   .catch(
     (err) => {
